cef3f46767dac7a7f97c554f7432e7e8692f3163
hyj8832 <hyj8832@naver.com>
2017-05-29 19:15:04 +0900

    #5 DialogInterface.OnClickListner

--- app/src/main/java/com/example/admin/dialogtest/MainActivity.java
+++ app/src/main/java/com/example/admin/dialogtest/MainActivity.java
@@ -36,16 +36,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });*/
 
         //라디오 버튼 목록
-        dialog.setSingleChoiceItems(itemArr,0,new DialogInterface.OnClickListener() {
+    /*    dialog.setSingleChoiceItems(itemArr,0,new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 butDialog.setText(itemArr[i]);
 
             }
+        });*/
+        //체크 박스 목록
+//      선택 여부 배열
+        //음성녹음 해둠
+        boolean[] checkedItems={true,false,true,false};
+        dialog.setMultiChoiceItems(itemArr, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i, boolean b){
+                if(b)
+                butDialog.setText(itemArr[i]);
+            }
         });
+
         // dialog.setPositiveButton("OK",null);
         dialog.show();
-
     }
 }
 
